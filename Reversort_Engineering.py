402f51160b1966540211ca5251db3801bed0694a
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2021-04-03 14:02:46 +0530

    Update Reversort_Engineering.py

--- Reversort_Engineering.py
+++ Reversort_Engineering.py
@@ -4,13 +4,13 @@ tc = 1
 
 while t>0:
     n, c = map(int, input().split())
-    remaining = c - (n - 1)  # 6 - (4-1) = 3
-    l = list(range(1, n+1)) # [1,2,3,4]
+    remaining = c - (n - 1) 
+    l = list(range(1, n+1)) 
     if (c >= n-1 and c <= (n*(n+1)/2)-1):
-        for i in range(n-2, -1, -1):  #  [2, 1, 0]
-            tot_work = n - i - 1  # 1, 2, 3; 
-            used = min(remaining, tot_work) # min(3,1) ; min(2,2) ; min(0, 3)
-            remaining -= used # 2; 0
+        for i in range(n-2, -1, -1):  
+            tot_work = n - i - 1  
+            used = min(remaining, tot_work)
+            remaining -= used 
             l[i:i+used+1] = reversed(l[i:i+used+1])
         ans = ' '.join(str(a) for a in l)
     else:
